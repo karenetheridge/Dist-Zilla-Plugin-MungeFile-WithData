69bca57e3a1b93f2931456831e198aac15665063
Karen Etheridge <ether@cpan.org>
2013-08-13 17:27:48 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.017, github profile.

---
+++ lib/Dist/Zilla/Plugin/MungeFile/WithData.pm
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+package Dist::Zilla::Plugin::MungeFile::WithData;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::MungeFile::WithData;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-MungeFile-WithData>
+(or L<bug-Dist-Zilla-Plugin-MungeFile-WithData@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-MungeFile-WithData@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
