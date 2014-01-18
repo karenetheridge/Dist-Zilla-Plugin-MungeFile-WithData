2352ef83d48eb80e4553b957ef9ae4ec223fc5eb
Karen Etheridge <ether@cpan.org>
2014-01-18 14:48:49 -0800

    vim modeline

--- lib/Dist/Zilla/Plugin/MungeFile/WithData.pm
+++ lib/Dist/Zilla/Plugin/MungeFile/WithData.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::MungeFile::WithData;
 # ABSTRACT: Modify files in the build, with templates and DATA section
+# vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
 with (
