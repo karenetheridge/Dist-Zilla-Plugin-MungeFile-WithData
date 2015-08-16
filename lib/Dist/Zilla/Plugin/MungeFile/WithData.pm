f75974f2ae86194d7b37b91cc9dd34a68b6693d3
Karen Etheridge <ether@cpan.org>
2015-08-15 23:09:22 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/MungeFile/WithData.pm
+++ lib/Dist/Zilla/Plugin/MungeFile/WithData.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::MungeFile::WithData;
 # ABSTRACT: (DEPRECATED) Modify files in the build, with templates and DATA section
-# vim: set ts=8 sts=4 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.008';
 
